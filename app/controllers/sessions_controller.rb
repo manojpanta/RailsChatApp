904ebde5755d20350ec173b64e1168e6ec56a6a3
manojpanta <manojpanta95@gmail.com>
2019-07-28 00:50:26 -0600

    minor indentation fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      cookies[:user_id]   = user.id
+      cookies[:user_id] = user.id
       redirect_to user_path(user)
     else
       render :new
